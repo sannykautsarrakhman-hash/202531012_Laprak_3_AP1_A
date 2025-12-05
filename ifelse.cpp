f503b5a5b1be56774bd0a6e08aa7e662ba3502a7
sannykautsarrakhman-hash <sannykautsarrakhman@gmail.com>
2025-12-05 12:38:17 +0700

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int var = 1;
+    if(var >  0){
+        cout <<"Var= " << var << " bilangan positif.\n";
+    } else if (var == 0){
+        cout <<"Var= "<< var << " bilangan negatif.\n";
+    } else{ 
+        cout <<"Var= "<< var << "  bilangan psitif.\n";
+    }
+
+    return 0;
+}
